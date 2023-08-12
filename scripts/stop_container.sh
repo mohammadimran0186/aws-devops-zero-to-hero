a127240f8485aae1ed418715f3a90cbd41b2e0eb
mohammadimran0186 <141118387+mohammadimran0186@users.noreply.github.com>
2023-08-12 12:35:38 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-# Stop the running  container (if any)
-containerid='docker ps | awk -F " " '{print $1}''
-docker rm -f $containerid
+# Stop the running  container (if any
+containerid= docker ps | awk -F " " '{print $1}'
+docker rm -rf $containerid
